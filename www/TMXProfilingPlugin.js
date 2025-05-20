ab0a7a61ee33780a9e44b325eb36b2164d408ef4
pcamilojunior <paulo.camilo.junior@outsystems.com>
2025-05-20 11:23:30 -0300

    add profiling arguments

--- www/TMXProfilingPlugin.js
+++ www/TMXProfilingPlugin.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.init = function (orgID, fpServer, registerForLocationServices, screenOffTimeout, disableLocSerOnBatteryLow, profileTimeout, disableNonfatalLogs, success, error) {
-    exec(success, error, 'TMXProfilingPlugin', 'init', [orgID, fpServer, registerForLocationServices, screenOffTimeout, disableLocSerOnBatteryLow, profileTimeout, disableNonfatalLogs]);
+exports.init = function (orgID, fpServer, registerForLocationServices, screenOffTimeout, disableLocSerOnBatteryLow, profileTimeout, disableNonfatalLogs, connectionTimeout, retryTimes, success, error) {
+    exec(success, error, 'TMXProfilingPlugin', 'init', [orgID, fpServer, registerForLocationServices, screenOffTimeout, disableLocSerOnBatteryLow, profileTimeout, disableNonfatalLogs, connectionTimeout, retryTimes]);
 };
 
 exports.doProfile = function (sessionID, success, error) {
